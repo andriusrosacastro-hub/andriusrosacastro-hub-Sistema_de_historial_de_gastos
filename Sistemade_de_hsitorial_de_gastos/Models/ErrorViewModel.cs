e7aa249608dabb04562c737d5a585ac6c2e5a305
Andrius Rosa <andrius.rosa@solvex.com.do>
2026-04-01 22:13:44 -0400

    Agregar archivos de proyecto.

---
+++ Sistemade_de_hsitorial_de_gastos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistemade_de_hsitorial_de_gastos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
