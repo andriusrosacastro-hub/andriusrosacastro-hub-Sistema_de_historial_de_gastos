e1c54fa5c57cfcf3d06eaf5bfa9e64deffddba10
Andrius Rosa <andrius.rosa@solvex.com.do>
2026-04-03 20:07:19 -0400

    Se creo el controlador GastosController dentro de la carpeta Controllers/ con las 8 acciones del CRUD: Index (listar), Details (detalle), Create GET/POST (crear), Edit GET/POST (editar), Delete GET/POST (eliminar). El controlador recibe GastoData mediante inyección de dependencias y utiliza validación con ModelState y [ValidateAntiForgeryToken].

---
+++ Sistemade_de_hsitorial_de_gastos/Controllers/GastosController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistemade_de_hsitorial_de_gastos.Data;
+using Sistemade_de_hsitorial_de_gastos.Models;
+
+namespace Sistemade_de_hsitorial_de_gastos.Controllers
+{
+    public class GastosController : Controller
+    {
+        private readonly GastoData _gastoData;
+
+        public GastosController(GastoData gastoData)
+        {
+            _gastoData = gastoData;
+        }
+
+        // GET: Gastos
+        public IActionResult Index()
+        {
+            var lista = _gastoData.ObtenerGastos();
+            return View(lista);
+        }
+
+        // GET: Gastos/Details/5
+        public IActionResult Details(int id)
+        {
+            var gasto = _gastoData.ObtenerGastoPorId(id);
+
+            if (gasto == null)
+            {
+                return NotFound();
+            }
+
+            return View(gasto);
+        }
+
+        // GET: Gastos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Gastos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Gasto gasto)
+        {
+            if (ModelState.IsValid)
+            {
+                _gastoData.InsertarGasto(gasto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(gasto);
+        }
+
+        // GET: Gastos/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var gasto = _gastoData.ObtenerGastoPorId(id);
+
+            if (gasto == null)
+            {
+                return NotFound();
+            }
+
+            return View(gasto);
+        }
+
+        // POST: Gastos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Gasto gasto)
+        {
+            if (id != gasto.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _gastoData.ActualizarGasto(gasto);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(gasto);
+        }
+
+        // GET: Gastos/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var gasto = _gastoData.ObtenerGastoPorId(id);
+
+            if (gasto == null)
+            {
+                return NotFound();
+            }
+
+            return View(gasto);
+        }
+
+        // POST: Gastos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _gastoData.EliminarGasto(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
